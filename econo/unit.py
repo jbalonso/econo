cc00c501d269426309b99f79a945a8149c094b8e
Jason Bernardino Alonso <jalonso@lumino.so>
2013-09-25 19:44:16 -0400

    (unit) step_time(): iterate over all units and apply market effects

--- econo/unit.py
+++ econo/unit.py
@@ -13,7 +13,7 @@ A Unit's state is a dictionary with the following structure:
 import logging; logger = logging.getLogger(__name__)
 from collections import namedtuple, defaultdict
 
-from .market import sell, buy, price_op
+from .market import sell, buy, price_op, ask_at
 
 # The Op class takes three components: costs, products, and time
 # - costs: dictionary mapping resource names to quantities consumed
@@ -90,3 +90,65 @@ def spawn_unit(t, careers, units, parent):
     units[name] = dict(age=0, busy=0, career=career, balance=0, name=name)
     logger.info('t=%60d: %r gives birth to %r', t, parent, name)
     return name
+
+def step_time(t, market, careers, units, rate, min_balance=-100, max_age=1000,
+        eat_every=100, spawn_every=200):
+    """
+    """
+    # Iterate over all units in order of balance
+    unit_list = [(k, v) for k, v in units.items()]
+    unit_list.sort(key=lambda x: x[1]['balance'], reverse=True)
+    for key, unit_state in unit_list:
+        # Eat if necessary
+        if (unit_state['age'] % eat_every) == 0:
+            cost = ask_at(market, 'food')
+            if (unit_state['balance'] - cost) < min_balance:
+                logger.warn('t=%60d: unit %(name)r (age %(age)d) starved', t,
+                        unit_state)
+                units.pop(key)
+            else:
+                unit_state['balance'] -= cost
+                buy(market, 'food')
+                spawn_unit(careers, units, key)
+
+        # Spawn if appropriate and able
+        if (unit_state['age'] % spawn_every) == 0:
+            cost = ask_at(market, 'babykit')
+            if (unit_state['balance'] - cost) < min_balance:
+                logger.warn('t=%60d: unit %(name)r (age %(age)d) could not
+                        spawn', t, unit_state)
+                units.pop(key)
+            else:
+                unit_state['balance'] -= cost
+                buy(market, 'babykit')
+                spawn_unit(t, careers, units, key)
+
+        # Choose and perform an operation
+        if unit_state['busy'] == 0:
+            ops = careers[unit_state['career']]['ops']
+            balance = unit_state['balance']
+            max_time = max_age - unit_stage['age']
+            op = choose_op(market, ops, rate, balance, min_balance, max_time)
+            perform_op(market, unit_state, op)
+        else:
+            unit_state['busy'] -= 1
+
+        # Age the unit
+        unit_state['age'] += 1
+        if unit_state['age'] >= max_age:
+            logger.info('t=%60d: unit %r dies of old age', t, key)
+
+    # Compute avg_earnings per career and other aggregate stats
+    for career_rec in careers.values():
+        career_rec['total_balance'] = 0.0
+        career_rec['total_age'] = 0
+        career_rec['population'] = 0
+    for key, unit_state in unit_list:
+        career = unit_state['career']
+        careers[career]['total_balance'] += unit_state['balance']
+        careers[career]['total_age'] += unit_state['age']
+        careers[career]['population'] += 1
+    for career, career_rec in careers.items():
+        career_rec['avg_earnings'] = (career_rec['total_balance'] /
+                career_rec['total_age'])
+        logger.info('t=%60d: career %s: %r', t, career, career_rec)
