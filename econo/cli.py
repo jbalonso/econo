3cf4c68d50e00113c041def81a575dd5683a1aab
Jason Bernardino Alonso <jalonso@lumino.so>
2013-09-25 20:12:03 -0400

    (cli) parse input yaml files

--- econo/cli.py
+++ econo/cli.py
@@ -2,7 +2,8 @@
 econo.cli -- CLI tools for the econo economic simulation
 """
 import logging; logger = logging.getLogger(__name__)
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
+from yaml import safe_load as load_yaml
 
 VERSION = 0.1
 
@@ -19,6 +20,11 @@ def main():
 
     # Handle the run command
     subparser = subparsers.add_parser('run', help=cmd_run.__doc__)
+    subparser.add_argument('-s', '--steps', type=int, default=10000,
+            help='Number of steps to simulate (default %(default)d)')
+    subparser.add_argument('description-file', type=FileType, nargs='+',
+            help='One or more YAML files containing economic descriptions to be'
+                 'read in order')
     subparser.set_defaults(func=cmd_run)
 
     # Parse options
@@ -35,4 +41,10 @@ def cmd_run(args):
     """
     Simulate an economy for a specified number of steps
     """
-    pass
+    description = {}
+    for desc_file in args.description_file:
+        with desc_file:
+            description.update(load_yaml(desc_file))
+
+    # TODO: Validate data structures
+    # TODO: Reconstruct economy and run it
